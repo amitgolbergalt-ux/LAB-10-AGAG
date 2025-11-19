73e542a1290eade9cd2428064ff148d3fa169cc3
golbe <amitgolberg@gmail.com>
2025-11-19 12:52:12 -0500

    Changed on other Computer

--- calculator.py
+++ calculator.py
@@ -9,19 +9,19 @@ import math
 
 def add(a, b): 
     return a+b
-def subtract(a,b):
+def sub(a,b):
     return a-b
-def multiply(a,b):
+def mul(a,b):
     return a*b
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return a/b
-def logarithm(a,b):
+def log(a,b):
     if b <= 0:
         raise ValueError
     return math.log(b,a)
-def exponent(a,b):
+def exp(a,b):
     return a**b
 
 
